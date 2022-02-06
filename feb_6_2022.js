de5b7669f2f089c3487ca9f7538e061d1d6928e4
nraff <nraff510@gmail.com>
2022-02-06 11:16:25 -0800

    part 2 of algo practice

--- feb_6_2022.js
+++ feb_6_2022.js
@@ -14,3 +14,29 @@ function getNodes(head) {
     return [head]
   }
 }
+
+// * Delete the node n spaces from the end
+var removeNthFromEnd = function (head, n) {
+  // flatten the nodes to an array
+  const nodes = getNodes(head)
+  const removeNodeIdx = nodes.length - n
+  if (nodes.length - n > 0) {
+    // get the node n + 1 spaces from the end
+    const beforeNodeIdx = nodes.length - n - 1
+    // set that nodes next value to be the node after n
+    const afterNodeIdx = nodes.length - n + 1
+    nodes[beforeNodeIdx].next = nodes[afterNodeIdx] ? nodes[afterNodeIdx] : null
+    return nodes[0]
+  } else {
+    nodes.splice(removeNodeIdx, 1)
+    return nodes[0] ? nodes[0] : new ListNode(undefined, null).next
+  }
+
+};
+
+function getNodes(head) {
+  if (head.next !== null) {
+    return [head].concat(getNodes(head.next))
+  }
+  return [head]
+}
